f01769c063cd2826df4823f25144081a2793815d
ca203 <universe.cao@gmail.com>
2022-05-09 01:15:33 +1000

    Initial Commit

---
+++ Ticket-ordering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ticket-ordering
+//
+//  Created by 陈雨欣 on 2022/5/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
