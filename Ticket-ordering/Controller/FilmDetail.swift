141b03592db449d1e58208112bc839f67a741c12
gongming <gm.shi@outlook.com>
2022-05-20 22:04:35 +1000

    finallll!

--- Ticket-ordering/Controller/FilmDetail.swift
+++ Ticket-ordering/Controller/FilmDetail.swift
@@ -29,15 +29,30 @@ class FilmsDetailController: UIViewController
     @IBOutlet var filmDescription: UITextView!
     @IBOutlet var castLabel: UILabel!
     @IBOutlet var directorLabel: UILabel!
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet var durationLB: UILabel!
+    @IBOutlet var genreLB: UILabel!
+    @IBOutlet var ratingLB: UILabel!
+    @IBOutlet var directorLB: UILabel!
+
+    @IBOutlet var imageFrame: UIView!
+    @IBOutlet var buyTicketButton: UIButton!
+    @IBOutlet var castLB: UILabel!
     var filmDetails: FilmDetails? {
         didSet {
             configure()
         }
     }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        for button in buttons {
+            button.layer.cornerRadius = 5
+        }
+        imageFrame.layer.cornerRadius = 10
         configure()
+        
     }
     
     func configure() {
@@ -47,7 +62,23 @@ class FilmsDetailController: UIViewController
                 titleLabel.text = filmDetails.film_name
                 filmDescription.text = filmDetails.synopsis_long
                 filmDescription.isEditable = false
-                
+                durationLB.text = "Duration: \(filmDetails.duration_mins) mins"
+                genreLB.text = "Genre: \(filmDetails.genres[0].genre_name)"
+                ratingLB.text = "Rating: \(filmDetails.review_stars) ⭐"
+                var casts = ""
+                filmDetails.cast.forEach({ cast in
+                    casts.append("\(cast.cast_name), ")
+                }) 
+                castLB.text = "Cast: \(casts)"
+                var directors = ""
+                filmDetails.directors.forEach({ director in
+                    directors.append("\(director.director_name), ")
+                })
+                directorLB.text = "Directors: \(directors)"
+                filmImage.imageFromServerURL(urlString:  filmDetails.images.poster?.one.medium.film_image ?? "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg", PlaceHolderImage: UIImage.init(named: "placeHolder")!)
+                if filmDetails.show_dates.isEmpty {
+                    buyTicketButton.isHidden = true
+                }
             }
             
         }
